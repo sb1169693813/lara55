b3a7e6ebabf2a46ac4676af08d6502ba16a5126d
sb1169693813 <446352377@qq.com>
2018-01-09 12:47:59 +0000

    finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title','lara55.app')</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </body>
 </html>
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','用户注册')
+@section('content')
+  <h1>zhuce</h1>
+@stop
